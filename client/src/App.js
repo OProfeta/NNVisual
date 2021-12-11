5aab43f684db5c9eed6644b5f450f69bafe2676b
O Profeta <matheuscesar98@gmail.com>
2021-12-11 03:51:30 -0300

    fix import in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react'
 import ReactFlow from 'react-flow-renderer'
-import ModelMenu from './components/ModelMenu/ModelMenu';
 import './App.css'
 
 function App() {
