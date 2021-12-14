4694b2defad5896b92b18af455f48f33cae7c7a9
O Profeta <matheuscesar98@gmail.com>
2021-12-13 22:49:58 -0300

    Creates items for the navbar

---
+++ client/src/components/ModelMenu/ModelMenuItems.js
@@ -0,0 +1,18 @@
+export const ModelMenuItems = [
+    {
+        title: "Processing",
+        cName: "code-block-selector",
+    },
+    {
+        title: "Deep Learning",
+        cName: "code-block-selector",
+    },
+    {
+        title: "Operations",
+        cName: "code-block-selector",
+    },
+    {
+        title: "Custom",
+        cName: "code-block-selector",
+    }
+]
