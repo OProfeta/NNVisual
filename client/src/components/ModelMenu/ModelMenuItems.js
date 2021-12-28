1ad804c20d3ab61f81d5e07e88b9b2cb07a8b360
O Profeta <matheuscesar98@gmail.com>
2021-12-28 15:52:00 -0300

    Create dropdown in navbar

--- client/src/components/ModelMenu/ModelMenuItems.js
+++ client/src/components/ModelMenu/ModelMenuItems.js
@@ -1,90 +1,81 @@
 export const ModelMenuItems = [
     {
+        id: 0,
         title: "Processing",
-        cName: "code-block-selector",
-        onClick: (elements, setElements) => {
-            setElements((elements) => {
-                return [
-                    ...elements, 
-                    {
-                        id: elements.length.toString(),
-                        type: 'codeNode',
-                        data: { 
-                            id: elements.length.toString(), 
-                            color: '#333' ,
-                            placeHolder: 'Code goes here'
-                        },
-                        style: { border: '1px solid #777', padding: 10 },
-                        position: { x : 251, y: 251}
-                    }
-                ]
-            });
-        },
+        selected: false,
+        key: "processing",
+        list: [
+            {
+                title: "Reshape"
+            },
+            {
+                title: "Grayscale"
+            },
+            {
+                title: "OneHot"
+            },
+            {
+                title: "Rescale"
+            },
+        ]
     },
     {
+        id: 1,
         title: "Deep Learning",
-        cName: "code-block-selector",
-        onClick: (elements, setElements) => {
-            setElements((elements) => {
-                return [
-                    ...elements,
-                    {
-                        id: elements.length.toString(),
-                        type: 'codeNode',
-                        data: { 
-                            id: elements.length.toString(), 
-                            color: '#333' ,
-                            placeHolder: 'Code goes here'
-                        },
-                        style: { border: '1px solid #777', padding: 10 },
-                        position: { x : 251, y: 251}
-                    }
-                ]
-            });
-        },
+        selected: false,
+        key: "deepLearning",
+        list: [
+            {
+                title: "Dense"
+            },
+            {
+                title: "Convolution"
+            },
+            {
+                title: "Recurrent"
+            },
+        ]
     },
     {
+        id: 2,
         title: "Operations",
-        cName: "code-block-selector",
-        onClick: (elements, setElements) => {
-            setElements((elements) => {
-                return [
-                    ...elements,
-                    {
-                        id: elements.length.toString(),
-                        type: 'codeNode',
-                        data: { 
-                            id: elements.length.toString(), 
-                            color: '#333' ,
-                            placeHolder: 'Code goes here'
-                        },
-                        style: { border: '1px solid #777', padding: 10 },
-                        position: { x : 251, y: 251}
-                    }
-                ]
-            });
-        },
+        selected: false,
+        key: "operations",
+        list: [
+            {
+                title: "Argmax"
+            },
+            {
+                title: "Merge"
+            },
+        ]
     },
     {
+        id: 3,
         title: "Custom",
-        cName: "code-block-selector",
-        onClick: (elements, setElements) => {
-            setElements((elements) => {
-                return [
-                    ...elements,
-                    {
-                        id: elements.length.toString(),
-                        type: 'codeNode',
-                        data: { 
-                            id: elements.length.toString(), 
-                            color: '#333' ,
-                            placeHolder: 'Code goes here'
-                        },
-                        style: { border: '1px solid #777', padding: 10, backgroundColor: "white" },
-                        position: { x : 251, y: 251}
-                    }
-                ]
-            });
-        },
-    }
+        selected: false,
+        key: "custom",
+        list: [
+            {
+                title: "Custom",
+                onClick: (elements, setElements) => {
+                    setElements((elements) => {
+                        return [
+                            ...elements,
+                            {
+                                id: elements.length.toString(),
+                                type: 'codeNode',
+                                data: {
+                                    id: elements.length.toString(),
+                                    placeHolder: "Code goes here"
+                                },
+                                style: { border: '1px solid #777', padding: 10 },
+                                position: { x: 251, y: 251 }
+                            }
+                        ]
+                    })
+                }
+            },
+        ]
+    },
 ]
