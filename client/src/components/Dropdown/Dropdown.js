c4cb3bedd22845c69d35c68dff6c7e6d828082b0
O Profeta <matheuscesar98@gmail.com>
2021-12-30 02:57:20 -0300

    Fix dropdowns in navbar

--- client/src/components/Dropdown/Dropdown.js
+++ client/src/components/Dropdown/Dropdown.js
@@ -2,7 +2,7 @@ import React, { useState } from "react"
 
 const hidden = false;
 
-function Dropdown({title, list}) {
+function Dropdown({title, list, elements, setElements}) {
 
     const [isHidden, setIsHidden] = useState(hidden);
 
@@ -11,11 +11,11 @@ function Dropdown({title, list}) {
             <button onClick={() => setIsHidden(!isHidden)}>
                 {title}
             </button>
-            {isHidden && <div style={{ backgroundColor: "white", position: "absolute" }}>
+            {isHidden && <div style={{ backgroundColor: "white" }}>
                 <ul style={{listStyle: "none", margin: "0", padding: "0"}}>
                     {list.map((item, index) => {
                         return (
-                            <li key={index} style={{padding: "8px 12px"}}>
+                            <li key={index} style={{padding: "8px 12px"}} onClick={() => item.onClick(elements, setElements)}>
                                 {item.title}
                             </li>
                         )
--- client/src/components/ModelMenu/ModelMenu.js
+++ client/src/components/ModelMenu/ModelMenu.js
@@ -9,13 +9,15 @@ function ModelMenu({elements, setElements}) {
             <h1>
                 Modeling Tool
             </h1>
-            <ul>
+            <ul style={{ display: "flex" }}>
                 {ModelMenuItems.map((item, index) => {
                     return (
                         <li key={index} style={{ position: "relative", display: "inline-block" }}>
                             <Dropdown
                                 title={item.title}
                                 list={item.list}
+                                elements={elements}
+                                setElements={setElements}
                             />
                         </li>
                     )
